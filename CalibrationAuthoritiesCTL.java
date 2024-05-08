d0c38881bd20edbd841028571be3bd120bb7d0ba
Vaishnavi Raghav <132844893+Vaishnavi-raghav@users.noreply.github.com>
2024-05-08 20:48:32 +0530

    Add files via upload

---
+++ CalibrationAuthoritiesCTL.java
@@ -0,0 +1,44 @@
+package dccweb.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TimeAndLocationCTL
+ */
+@WebServlet(name = "CalibrationAuthoritiesCTL", urlPatterns = {"/CalibrationAuthoritiesCTL"})
+public class CalibrationAuthoritiesCTL extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CalibrationAuthoritiesCTL() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		RequestDispatcher rd = request.getRequestDispatcher(JWAView.DCC7View);
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
