aa6c3245ee39bc5a159c3963b4a2d8e382d32db4
Vaishnavi Raghav <132844893+Vaishnavi-raghav@users.noreply.github.com>
2024-05-08 17:39:50 +0530

    Add files via upload

---
+++ DCC2dateTimeLoc.java
@@ -0,0 +1,40 @@
+package dccweb.bean;
+
+import java.util.Date;
+
+public class DCC2dateTimeLoc {
+	private int RegNumber;
+	private String BeginDate;
+	private String EndDate;
+	private String Location;
+	
+	
+	public int getRegNumber() {
+		return RegNumber;
+	}
+	public void setRegNumber(int regNumber) {
+		RegNumber = regNumber;
+	}
+	public String getBeginDate() {
+		return BeginDate;
+	}
+	public void setBeginDate(String beginDate) {
+		BeginDate = beginDate;
+	}
+	public String getEndDate() {
+		return EndDate;
+	}
+	public void setEndDate(String endDate) {
+		EndDate = endDate;
+	}
+	public String getLocation() {
+		return Location;
+	}
+	public void setLocation(String location) {
+		Location = location;
+	}
+	
+	
+}
+	
+	
