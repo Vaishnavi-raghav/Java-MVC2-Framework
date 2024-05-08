cd593625c1f22cff71c035357319a64062ba71c5
Vaishnavi Raghav <132844893+Vaishnavi-raghav@users.noreply.github.com>
2024-05-08 21:09:18 +0530

    Add files via upload

---
+++ MeasurementResultsCTL.java
@@ -0,0 +1,44 @@
+package dccweb.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MeasurementResultsCTL
+ */
+@WebServlet(name = "MeasurementResultsCTL", urlPatterns = {"/MeasurementResultsCTL"})
+public class MeasurementResultsCTL extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MeasurementResultsCTL() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		RequestDispatcher rd = request.getRequestDispatcher(JWAView.DCC6View);
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
